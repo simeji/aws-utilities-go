3d040afa88dbd6140bc32104b708d5609c244293
simeji <simeji.net@gmail.com>
2015-05-11 01:32:36 +0900

    ver 0.1.0

--- commands.go
+++ commands.go
@@ -17,23 +17,9 @@ import (
 )
 
 var Commands = []cli.Command{
-	commandList_ipaddress,
 	commandExec_instance,
 	commandList_users,
-}
-
-var commandList_ipaddress = cli.Command{
-	Name:    "list-ipaddress",
-	Aliases: []string{"l"},
-	Usage:   "list ipaddress filtered by NameTag",
-	Description: `
-	aws-utilities -p {your_profile} l -n {NameTag} [-a]
-`,
-	Flags: []cli.Flag{
-		&cli.StringFlag{Name: "nametag, n", Usage: "NameTag [default: *]"},
-		&cli.BoolFlag{Name: "all, a", Usage: "Get all status instances"},
-	},
-	Action: doList_ipaddress,
+	commandOperate_instance,
 }
 
 var commandExec_instance = cli.Command{
@@ -63,6 +49,128 @@ var commandList_users = cli.Command{
 	Action: doList_users,
 }
 
+var commandOperate_instance = cli.Command{
+	Name:    "operate-instance",
+	Aliases: []string{"o", "op"},
+	Usage:   "operate instance start/stop filtered by NameTag",
+	Description: `
+	aws-utilities -p {your_profile} op -n {NameTag} -m {start/stop/status}
+`,
+	Flags: []cli.Flag{
+		&cli.StringFlag{Name: "nametag, n", Usage: "NameTag [default: *]"},
+		&cli.StringFlag{Name: "mode, m", Usage: "[*required] start/stop/status"},
+	},
+	Action: func(c *cli.Context) {
+
+		name, err := getNametag(c)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		mode, merr := getMode(c)
+		if merr != nil {
+			fmt.Println(merr)
+			os.Exit(1)
+		}
+
+		profile := getProfile(c)
+		cred := getCredential(profile)
+		svc := ec2.New(getConfig(cred))
+
+		status := getModeStatus(mode)
+
+		params := &ec2.DescribeInstancesInput{
+			Filters: []*ec2.Filter{
+				&ec2.Filter{
+					Name: aws.String("tag:Name"),
+					Values: []*string{
+						aws.String(name),
+					},
+				},
+			},
+		}
+
+		if status != "" {
+			params.Filters = append(params.Filters,
+				&ec2.Filter{
+					Name: aws.String("instance-state-name"),
+					Values: []*string{
+						aws.String(status),
+					},
+				},
+			)
+		}
+
+		res, err := svc.DescribeInstances(params)
+
+		if awserr := aws.Error(err); awserr != nil {
+			// A service error occurred.
+			fmt.Println("Error:", awserr.Code, awserr.Message)
+			os.Exit(1)
+		} else if err != nil {
+			// A non-service error occurred.
+			panic(err)
+			os.Exit(1)
+		}
+
+		var instanceIds []*string
+
+		for _, r := range res.Reservations {
+			for _, i := range r.Instances {
+				var nt, privateip, publicip string
+				for _, t := range i.Tags {
+					if *t.Key == "Name" {
+						nt = *t.Value
+						break
+					}
+				}
+				if i.PrivateIPAddress != nil {
+					privateip = *i.PrivateIPAddress
+				}
+				if i.PublicIPAddress != nil {
+					publicip = *i.PublicIPAddress
+				}
+				fmt.Println(
+					nt,
+					privateip,
+					publicip,
+					*i.InstanceID,
+					*i.State.Name,
+				)
+				instanceIds = append(instanceIds, i.InstanceID)
+			}
+		}
+
+		if len(instanceIds) < 1 {
+			fmt.Println("No " + status + " instances..Search by [" + name + "]")
+			os.Exit(0)
+		}
+
+		switch mode {
+		case "start":
+			input := &ec2.StartInstancesInput{
+				InstanceIDs: instanceIds,
+			}
+			sOut, sErr := svc.StartInstances(input)
+			if err := formatAwsError(sErr); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(awsutil.StringValue(sOut))
+		case "stop":
+			input := &ec2.StopInstancesInput{
+				InstanceIDs: instanceIds,
+			}
+			sOut, sErr := svc.StopInstances(input)
+			if err := formatAwsError(sErr); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(awsutil.StringValue(sOut))
+		}
+	},
+}
+
 func getProfile(c *cli.Context) (profile string) {
 	profile = c.GlobalString("profile")
 	if profile == "" {
@@ -73,6 +181,31 @@ func getProfile(c *cli.Context) (profile string) {
 
 func getCredential(profile string) *credentials.Credentials {
 	return credentials.NewSharedCredentials("", profile)
+}
+
+func getMode(c *cli.Context) (mode string, err error) {
+	err = nil
+	modes := map[string]bool{
+		"start":  true,
+		"stop":   true,
+		"status": true,
+	}
+	mode = c.String("mode")
+	if mode == "" {
+		err = fmt.Errorf("'--mode' is required")
+	} else if !modes[mode] {
+		err = fmt.Errorf("--mode must be included in [start|stop|status]")
+	}
+	return
+}
+
+func getModeStatus(mode string) string {
+	modes := map[string]string{
+		"start":  "stopped",
+		"stop":   "running",
+		"status": "",
+	}
+	return modes[mode]
 }
 
 func getNametag(c *cli.Context) (name string, err error) {
@@ -88,75 +221,16 @@ func getConfig(cred *credentials.Credentials) *aws.Config {
 	return &aws.Config{Credentials: cred, Region: "ap-northeast-1"}
 }
 
-func doList_ipaddress(c *cli.Context) {
-	name, err := getNametag(c)
-
-	if name == "" {
-		name = "*"
-	}
-
-	profile := getProfile(c)
-	cred := getCredential(profile)
-	svc := ec2.New(getConfig(cred))
-
-	params := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
-			&ec2.Filter{
-				Name: aws.String("tag:Name"),
-				Values: []*string{
-					aws.String(name),
-				},
-			},
-		},
-	}
-
-	if c.Bool("all") == false {
-		sf := &ec2.Filter{
-			Name: aws.String("instance-state-name"),
-			Values: []*string{
-				aws.String("running"),
-			},
-		}
-		params.Filters = append(params.Filters, sf)
-	}
-
-	res, err := svc.DescribeInstances(params)
-
+func formatAwsError(err error) error {
+	var aerr error = nil
 	if awserr := aws.Error(err); awserr != nil {
 		// A service error occurred.
-		fmt.Println("Error:", awserr.Code, awserr.Message)
-		os.Exit(1)
+		aerr = fmt.Errorf("Error:", awserr.Code, awserr.Message)
 	} else if err != nil {
 		// A non-service error occurred.
-		panic(err)
-		os.Exit(1)
+		aerr = aerr
 	}
-
-	for _, r := range res.Reservations {
-		for _, i := range r.Instances {
-			var nt string
-			for _, t := range i.Tags {
-				if *t.Key == "Name" {
-					nt = *t.Value
-					break
-				}
-			}
-			var privateip, publicip string
-			if i.PrivateIPAddress != nil {
-				privateip = *i.PrivateIPAddress
-			}
-			if i.PublicIPAddress != nil {
-				publicip = *i.PublicIPAddress
-			}
-			fmt.Println(
-				nt,
-				privateip,
-				publicip,
-				*i.InstanceID,
-				*i.State.Name,
-			)
-		}
-	}
+	return aerr
 }
 
 func doList_users(c *cli.Context) {
